c72eafd896fd504a09f4023a9eca7122d73c335d
crumwj22 <crumwj22@hotmail.com>
2022-04-23 15:45:25 -0700

    updated index.js with prompts, started generateReadme function

--- index.js
+++ index.js
@@ -1,45 +1,67 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+// how to generate README
 
 inquirer
   .prompt([
     {
-      type: 'input',
-      message: 'name of project',
-      name: 'What the name of your project?',
+      type: "input",
+      message: "<Your-Project-Title>",
+      name: "projectTitle",
+    },
+
+    {
+      type: "input",
+      message: "Project Description",
+      name: "projectDescription",
     },
     {
-        type: 'input',
-        message: 'description',
-        name: 'Project Description',
-      },
-      {
-        type: 'input',
-        message: 'installation',
-        name: 'How to Install and Run the Project',
-      },
-      {
-        type: 'input',
-        message: 'Project Use',
-        name: 'How to use the project',
-      },
-      {
-        type: 'input',
-        message: 'Credits',
-        name: 'Project Credits',
-      },
-      {
-        type: 'input',
-        message: 'License',
-        name: 'Project License',
-      },
-    
+      type: "input",
+      message: "Table of Contents",
+      name: "tableOfContents",
+    },
+    {
+      type: "input",
+      message: "What are the steps required to install your project?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message:
+        "Provide instructions and examples for use. Include screenshots as needed.",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "List your collaborators",
+      name: "collaborators",
+    },
+    {
+      type: "input",
+      message: "List your license",
+      name: "license",
+    },
   ])
-  .then(function (response){
-    console.log(response);
+  .then((answers) => {
+    const readmePageContent = generateReadme(answers);
 
-  fs.writeFile("user-information.json", JSON.stringify(response), function (err){
-  err ? console.error(err) : console.log("success!")
-    }
-  );
-});
+    fs.writeFile("testREADME.md", readmePageContent, (err) =>
+      err
+        ? console.log(err)
+        : console.log("Successfully created testREADME.md!")
+    );
+  });
+const generateReadme = (answers) => {
+  let result = `
+    # ${answers.projectTitle}
+    ## ${answers.projectDescription}
+    ## ${answers.tableOfContents}
+    ## ${answers.installation}
+    ## ${answers.usage}
+    ## ${answers.collaborators}
+    ## ${answers.license}
+    
+    `;
+  return result;
+};
---
+++ testREADME.md
@@ -0,0 +1,10 @@
+
+    # statis titlejesse
+    ## aljdfl;aj
+    ## alsj;dfl;ja
+    ## asl;djfasfj
+    ## a;jdf;lash
+    ## a;sjfhas
+    ## ;asfj;alj
+    
+    
