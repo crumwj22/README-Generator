50561ae16181e6110f77a964d321557fe35c0a00
crumwj22 <crumwj22@hotmail.com>
2022-04-23 17:07:40 -0700

    updated index.js with additional readme promps and validators

--- index.js
+++ index.js
@@ -1,46 +1,115 @@
+// Declaring the dependancies and variables
 const fs = require("fs");
 const inquirer = require("inquirer");
 
-// how to generate README
-
+// Prompt the user questions to populate the README.md
 inquirer
   .prompt([
     {
       type: "input",
-      message: "<Your-Project-Title>",
+      message: "What is the title of your project?",
       name: "projectTitle",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter the title of your project");
+          return false;
+        }
+      },
     },
 
     {
       type: "input",
-      message: "Project Description",
+      message: "Please write a short description of your project",
       name: "projectDescription",
-    },
-    {
-      type: "input",
-      message: "Table of Contents",
-      name: "tableOfContents",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please write a short discription of your project");
+          return false;
+        }
+      },
     },
     {
       type: "input",
       message: "What are the steps required to install your project?",
       name: "installation",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter steps required to install your project");
+          return false;
+        }
+      },
     },
     {
       type: "input",
-      message:
-        "Provide instructions and examples for use. Include screenshots as needed.",
+      message: "What is this project used for?",
       name: "usage",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter what this project will be used for");
+          return false;
+        }
+      },
     },
     {
       type: "input",
       message: "List your collaborators",
       name: "collaborators",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please list the people who contributed to this project");
+          return false;
+        }
+      },
+    },
+    {
+      type: "list",
+      message: "Choose the appropriate license for this project: ",
+      name: "license",
+      choices: ["Apache", "Academic", "GNU", "ISC", "MIT", "Mozilla", "Open"],
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please choose a license from the list");
+          return false;
+        }
+      },
     },
     {
       type: "input",
-      message: "List your license",
-      name: "license",
+      message: "Please enter your GitHub username",
+      name: "userName",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub username");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "Please enter your email address",
+      name: "email",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your email address");
+          return false;
+        }
+      },
     },
   ])
   .then((answers) => {
@@ -52,16 +121,32 @@ inquirer
         : console.log("Successfully created testREADME.md!")
     );
   });
+
+// Write new README.md to dist directory
 const generateReadme = (answers) => {
   let result = `
-    # ${answers.projectTitle}
-    ## ${answers.projectDescription}
-    ## ${answers.tableOfContents}
-    ## ${answers.installation}
-    ## ${answers.usage}
-    ## ${answers.collaborators}
-    ## ${answers.license}
-    
+  # ${answers.projectTitle}
+  
+  ## Project Description: 
+  ${answers.projectDescription}
+  
+  ## Steps required to install your project:
+  ${answers.installation}
+  
+  ## What will this project be used for:
+  ${answers.usage}
+  
+  ## Project Collaborators:
+  ${answers.collaborators}
+  
+  ## License:
+  ${answers.license}
+  
+  ## GitHub username: 
+  ${answers.userName}
+  
+  ## Email Address:
+  ${answers.email}
     `;
   return result;
 };
--- testREADME.md
+++ testREADME.md
@@ -1,10 +1,24 @@
 
-    # statis titlejesse
-    ## aljdfl;aj
-    ## alsj;dfl;ja
-    ## asl;djfasfj
-    ## a;jdf;lash
-    ## a;sjfhas
-    ## ;asfj;alj
-    
+  # README generator
+  
+  ## Project Description: 
+  this is a program to generate a professional README file.
+  
+  ## Steps required to install your project:
+  you need to install npm
+  
+  ## What will this project be used for:
+  generating a professional README
+  
+  ## Project Collaborators:
+  myself
+  
+  ## License:
+  Apache
+  
+  ## GitHub username: 
+  Crumwj22
+  
+  ## Email Address:
+  crumwj22@hotmail.com
     
